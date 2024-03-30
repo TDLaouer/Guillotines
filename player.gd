609c1c541cf4dccedb3a1c2e68d441947d882f9f
FayssalM <fayssalmoumni@gmail.com>
2024-03-30 19:31:44 +0100

    [GAME] Player type updated (CharacterBody2D) and physics "improved"

--- player.gd
+++ player.gd
@@ -1,40 +1,58 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-@export var speed = 400 # Vitesse du player en pixels/sec
+@export var move_speed = 400 # Vitesse du player en pixels/sec
+@export var jump_height : float
+@export var jump_time_to_peak : float
+@export var jump_time_to_descent : float
+
 var screen_size
 
 # Methode appelee lorsque l'objet "entre dans le scene tree" pour la premiere fois
 func _ready():
+	velocity = Vector2.ZERO
 	screen_size = get_viewport_rect().size
 
 # Methode appelee à chaque frame calculee
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	
-	if Input.is_anything_pressed() == false:
-		$AnimatedSprite2D.animation = "idle"
+func _physics_process(delta):
+	velocity.y += get_gravity() * delta
+	velocity.x = get_input_velocity() * move_speed
+
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play()
 	else:
-		if Input.is_action_pressed("move_right"):
-			velocity = velocity.move_toward(Vector2.RIGHT,delta)
-		if Input.is_action_pressed("move_left"):
-			velocity = velocity.move_toward(Vector2.LEFT,delta)
-		if Input.is_action_pressed("move_up"):
-			velocity = velocity.move_toward(Vector2.UP,delta)
+		$AnimatedSprite2D.stop()
 
-		if velocity.length() > 0:
-			velocity = velocity.normalized() * speed
-			$AnimatedSprite2D.play()
-		else:
-			$AnimatedSprite2D.stop()
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite2D.animation = "idle"
+		
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
+		jump()
 
-		position += velocity * delta
-		position = position.clamp(Vector2.ZERO, screen_size)
+	move_and_slide()
 
-		if velocity.x != 0:
-			$AnimatedSprite2D.animation = "walk"
-			$AnimatedSprite2D.flip_v = false
-			# See the note below about boolean assignment.
-			$AnimatedSprite2D.flip_h = velocity.x < 0
+func jump():
+	velocity.y = ((2.0 * jump_height)  / jump_time_to_peak) * -1.0
+
+func get_gravity():
+	if velocity.y < 0:
+		return ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1
+	else: 
+		return ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1
+
+func get_input_velocity():
+	var horizontalVelocity := 0
+	
+	if Input.is_action_pressed("move_left"):
+		horizontalVelocity -= 1
+	if Input.is_action_pressed("move_right"):
+		horizontalVelocity += 1
+		
+	return horizontalVelocity
 
 # Methode appelee au demarrage/reset de la partie
 func start(pos):
